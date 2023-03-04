9f27be5e7a2929187980f0421fdcc7a2b569ab13
kamal heydari <kamal3heydari@gmail.com>
2023-03-04 19:44:22 +0330

    feat esLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./styles/global.css";
 import Image from "./assets/react-image.png";
 import Icon from "./assets/music.svg";
+import { Counter } from "./components/Counter";
+
 export default function App() {
+  
   return (
     <div>
       <h1>React typescript Webpack Starter Template</h1>
       <p>mode : <b>{process.env.NODE_ENV}</b></p>
       <img src={Image} alt='React Logo' width={450} height={250} />
       <img src={Icon} alt='Music Logo' />
+      <Counter/>
     </div>
   );
 }
